cd2559af2a28ecb1a8630db1ca277ee1544f1e99
ZareMendozaC <zare.mendozac@gmail.com>
2016-12-27 15:25:26 -0500

    adding read me, responsive-desing, animate result and background

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # JSparte2-ejercicio4
+# Ejercicio Nº 4
+# Inicio: Diff21
+## Ingreso de datos
+- numero
+- resultado
+## Proceso
+- Si el número ingresado es menor de 21, entonces 
+    - Obtener la resta de 21-numero;
+    - mostrar el resultado
+- Caso contrario
+    - Obtener el doble de la resta del numero menos 21, 2*(numero-21)
+    - mostrar el resultado
+
+*Fin*
---
+++ main.js
@@ -0,0 +1,23 @@
+function calcular()
+{
+    var numero= parseInt(document.getElementById("numero").value);
+    var respuesta = document.getElementById("respuesta");
+    var resultado;
+  if (numero<21){//si el número es menor que 21
+
+    resultado= 21-numero; // resto 21 menos el numero ingresado
+    // codigo html para mostrar el resultado
+    respuesta.innerHTML = '<h4>Es menor que 21, entonces: </h4><br>'+'<h4>21-'+numero+' es igual a:  </h4>'+'<h3 class="animated wobble result">'+resultado+'</h3>';
+  }
+    
+    else{
+
+        resultado= 2*(numero-21); //si no se resta el numero -21 y se le multiplica por dos
+        // codigo html para mostrar el resultado
+        respuesta.innerHTML = '<h4>Es mayor que 21, entonces: </h4><br>'+'<h4>2*('+numero+'-21) es igual a:  </h4>'+'<h3 class="animated wobble result">'+resultado+'</h3>';
+    }
+}
+function limpiar(){// esta funcion limpia el campo del número
+     numero = document.getElementById("numero").value="";
+}
+
